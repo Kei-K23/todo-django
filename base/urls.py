def44d013e7cb37ee5f42a5fac589f9d116beae0
Kei-K23 <keiksl2301@gmail.com>
2024-02-10 13:51:01 +0630

    add edit and delete feature

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('update/<str:pk>/', views.update, name='update'),
+    path('create/<str:pk>/', views.delete, name='delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 # Create your views here.
 
@@ -14,4 +14,22 @@ def create(request):
         todo_text = request.POST.get('todo')
         if todo_text:
             Todo.objects.create(todo=todo_text)
+            return redirect('index')
+
+
+def update(request, pk):
+    if request.POST.get('_method') == 'PUT':
+        todo_text = request.POST.get('todo')
+        if todo_text:
+            todo = Todo.objects.get(id=pk)
+            todo.todo = todo_text
+            todo.save()
+            return redirect('index')
+
+
+def delete(request, pk):
+    if request.POST.get('_method') == 'DELETE':
+        todo = Todo.objects.get(id=pk)
+        if todo:
+            todo.delete()
             return redirect('index')

ec1037babb59edf2adfea4046cadcd9b8396d11d
Kei-K23 <keiksl2301@gmail.com>
2024-02-10 10:12:53 +0630

    add model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    todo = models.CharField(max_length=255)
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
